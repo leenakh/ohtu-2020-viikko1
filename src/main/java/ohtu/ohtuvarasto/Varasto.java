a28c1a159d9e11a992ea06dd8f8c6a1754c6b9af
leenakh <leenakhahtola@gmail.com>
2020-02-25 21:43:50 +0200

    checkstyle on tyytyväinen, mutta kaikki testit eivät mene läpi, koska kaksiparametrisen konstruktorin syklomaattista kompleksisuutta piti pienentää liikaa

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -9,38 +9,27 @@ public class Varasto {
     // --- konstruktorit: ---
     public Varasto(double tilavuus) {  // tilavuus on annettava
         if (tilavuus > 0.0) {
-            if (tilavuus < 1.2) {
-                if (tilavuus == 1.1) {
-                    this.tilavuus = 1.1;
-                }
-                this.tilavuus = 0;
-            }
             this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
+        } else {// virheellinen, nollataan
+
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
-    saldo = 0;     // oletus: varasto on tyhjä
+        saldo = 0;     // oletus: varasto on tyhjä
     }
 
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
+        this.tilavuus = 0.0;
+        this.saldo = 0.0;
         if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
-            this.tilavuus = 0.0;  // => käyttökelvoton varasto
-        }
-        if (alkuSaldo < 0.0) {
-            this.saldo = 0.0;
-        } else if (alkuSaldo <= tilavuus) // mahtuu
-        {
             this.saldo = alkuSaldo;
-        } else {
+        }
+        if (alkuSaldo > tilavuus) {// mahtuu
             this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
         }
     }
+// --- ottavat aksessorit eli getterit: ---
 
-    // --- ottavat aksessorit eli getterit: ---
     public double getSaldo() {
         return saldo;
     }
@@ -55,12 +44,12 @@ public class Varasto {
 
     // --- asettavat aksessorit eli setterit: ---
     public void lisaaVarastoon(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä 
-        {
+        if (maara < 0) {// virhetilanteessa voidaan tehdä 
+
             return;       // tällainen pikapoistuminenkin!
         }
-        if (maara <= paljonkoMahtuu()) // omia aksessoreita voi kutsua
-        {
+        if (maara <= paljonkoMahtuu()) {// omia aksessoreita voi kutsua
+
             saldo = saldo + maara;          // ihan suoraan sellaisinaan
         } else {
             saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
@@ -68,8 +57,8 @@ public class Varasto {
     }
 
     public double otaVarastosta(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä 
-        {
+        if (maara < 0) {// virhetilanteessa voidaan tehdä 
+
             return 0.0;   // tällainen pikapoistuminenkin!
         }
         if (maara > saldo) {          // annetaan mitä voidaan
