8790f6ab4b7980a2080bf6f19d9501840ee790c8
leenakh <leenakhahtola@gmail.com>
2020-02-25 14:01:11 +0200

    viikko 2 tehtävä 1 Main-luokka

---
+++ viikko2/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kuka olet? ");
+        String nimi = scanner.nextLine();
+        System.out.println("Hello " + nimi + "!");
+    }
+}
